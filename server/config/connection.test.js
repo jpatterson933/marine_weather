3dbb82ebe8f1ce567c7659e285e9742c2aa6a7f8
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-17 11:52:31 -0700

    added mongo db connection testing. All tests pass. Test for connecting to the database and once databse is open, check to ensure a connection has been established

---
+++ server/config/connection.test.js
@@ -0,0 +1,22 @@
+const db = require('./connection');
+describe("Database Connection", () => {
+    
+    afterAll(() => {
+        db.close()
+    })
+    test("should be connecting to the database", async () => {
+        const readyState = db.readyState;
+        expect(readyState).toBe(2);
+    });
+    test("should be connected to the database", done => {
+        console.log(db)
+        db.once('open', () => {
+
+            const readyState = db.readyState;
+            expect(readyState).toBe(1);
+            done();
+        })
+    });
+
+
+})
