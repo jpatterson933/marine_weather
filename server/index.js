a9c20c809588ce11be232040a3fd45ad098ece47
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-17 11:40:25 -0700

    added semi colons and removed extra spaces

--- server/index.js
+++ server/index.js
@@ -1,26 +1,22 @@
 // setup db connection
-const db = require('./config/connection')
+const db = require('./config/connection');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
 
-
 // error catching for db connection
 db.on("error", (error) => {
-    console.log(error, "error with stuff")
-})
+    console.log(error, "error with stuff");
+});
 
 
 db.once("open", () => {
     app.on("session.destroy", (sessionId) => {
-        console.log(`Session ${sessionId} has expired!`)
-    })
-
+        console.log(`Session ${sessionId} has expired!`);
+    });
 
     //app running, not getting
     app.listen(PORT, function () {
         console.log(`🌎  ==> API Server now listening on http://localhost:${PORT}`, "SERVER LIVE");
     });
-
-
-})
+});
