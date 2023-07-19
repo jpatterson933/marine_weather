cfa4edcb267f7a973ee591a6048f6e1c7abd0690
jpatterson-cYRYJA <jpatterson933@ucla.edu>
2023-07-19 14:01:14 -0700

    testing for userName and userPassword to make sure errors are thrown if they are not given

---
+++ server/models/Surfer.test.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Surfer = require('./Surfer');
+
+const { connect, clearDatabase, closeDatabase } = require('../utils/db_test_setup');
+
+beforeAll(async () => await connect());
+afterEach(async () => await clearDatabase());
+afterAll(async () => await closeDatabase());
+
+describe("Surfer Model Test", () => {
+    it("create and save a surfer successfully", async () => {
+        const userData = {
+            userName: 'surfer123',
+            userPassword: 'testPassword123'
+        };
+
+        const validUser = new Surfer(userData);
+        const savedUser = await validUser.save();
+
+        // Object ID should be defined when saved to mongo
+        expect(savedUser._id).toBeDefined();
+        expect(savedUser.userName).toBe(userData.userName);
+
+        // password should be hased
+        expect(savedUser.userPassword).not.toBe(userData.userPassword);
+        expect(bcrypt.compareSync(userData.userPassword, savedUser.userPassword)).toBeTruthy();
+    });
+
+    it("should fail if userName does not exist", async () => {
+        const userData = {
+            userPassword: 'userPassword123',
+        };
+
+        const invalidUser = new Surfer(userData);
+
+        let error;
+        try {
+            const savedUser = await invalidUser.save()
+        } catch(e){
+            error = e;
+        }
+        expect(error).toBeDefined();
+        expect(error.message).toContain("userName")
+    });
+
+    
+    it("should fail if userPassword does not exist", async () => {
+        const userData = {
+            userName: 'surfer123',
+        };
+
+        const invalidUser = new Surfer(userData);
+
+        let error;
+        try {
+            const savedUser = await invalidUser.save()
+        } catch(e){
+            error = e;
+        }
+        expect(error).toBeDefined();
+        expect(error.message).toContain("userPassword")
+    })
+})
